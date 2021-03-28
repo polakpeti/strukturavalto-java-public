473802f2733f8d953ca5e79ddedc7c5d2cc97305
unknown <polakpeti@gmail.com>
2021-03-28 18:20:12 +0200

    works with google help

--- examples/zarovizsga-potpotvizsga/src/test/java/hu/nive/ujratervezes/zarovizsga/housecup/HouseCup.java
+++ examples/zarovizsga-potpotvizsga/src/test/java/hu/nive/ujratervezes/zarovizsga/housecup/HouseCup.java
@@ -1,10 +1,8 @@
 package hu.nive.ujratervezes.zarovizsga.housecup;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;;
+import java.sql.*;
+;
 
 public class HouseCup {
     private DataSource dataSource;
@@ -13,20 +11,20 @@ public class HouseCup {
         this.dataSource = dataSource;
     }
 
-    int getPointsOfHouse(String house){
+    int getPointsOfHouse(String house) {
         try (
                 Connection conn = dataSource.getConnection();
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery("select points_earned from house_points WHERE house_name = 'Gryffindor'; ")
+                PreparedStatement stmt = conn.prepareStatement("select points_earned from house_points WHERE house_name = ?; ");
         ) {
+            stmt.setString(1, house);
+            ResultSet rs = stmt.executeQuery();
             int counter = 0;
             while (rs.next()) {
                 String name = rs.getString("points_earned");
                 counter += Integer.parseInt(name);
             }
             return counter;
-        }
-        catch (SQLException se) {
+        } catch (SQLException se) {
             throw new IllegalStateException("Cannot select employees", se);
         }
     }
