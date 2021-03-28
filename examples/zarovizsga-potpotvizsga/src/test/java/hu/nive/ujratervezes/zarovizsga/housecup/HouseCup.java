b2302490ef3d4a9e0c3aca418240b425651f763c
unknown <polakpeti@gmail.com>
2021-03-28 18:13:50 +0200

    SQL query try

--- examples/zarovizsga-potpotvizsga/src/test/java/hu/nive/ujratervezes/zarovizsga/housecup/HouseCup.java
+++ examples/zarovizsga-potpotvizsga/src/test/java/hu/nive/ujratervezes/zarovizsga/housecup/HouseCup.java
@@ -1,16 +1,33 @@
 package hu.nive.ujratervezes.zarovizsga.housecup;
 
-import org.mariadb.jdbc.MariaDbDataSource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;;
 
 public class HouseCup {
-    MariaDbDataSource dataSource;
+    private DataSource dataSource;
 
-    public HouseCup(MariaDbDataSource dataSource) {
+    public HouseCup(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
     int getPointsOfHouse(String house){
-        int counter = 30;
-        return counter;
+        try (
+                Connection conn = dataSource.getConnection();
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery("select points_earned from house_points WHERE house_name = 'Gryffindor'; ")
+        ) {
+            int counter = 0;
+            while (rs.next()) {
+                String name = rs.getString("points_earned");
+                counter += Integer.parseInt(name);
+            }
+            return counter;
+        }
+        catch (SQLException se) {
+            throw new IllegalStateException("Cannot select employees", se);
+        }
     }
 }
